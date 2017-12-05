e1eca1cf09ce98af54ef75ad659f1f1897b42581
Michał Szyszka <mszyszka223@gmail.com>
2017-12-05 16:32:55 +0100

    I made sticky nav

--- script.js
+++ script.js
@@ -54,3 +54,29 @@
                 $.scrollTo($("#tech").offset().top -30,800);  
             });
         });
+
+   //Sticky .navContainer
+
+        $(document).ready(function() {
+			// grab the initial top offset of the navigation 
+		   	var stickyNavTop = $('.navContainer').offset().top;
+		   	
+		   	// our function that decides weather the navigation bar should have "fixed" css position or not.
+		   	var stickyNav = function(){
+			    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+			         
+			    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+			    // otherwise change it back to relative
+			    if (scrollTop > stickyNavTop) { 
+			        $('.navContainer').addClass('sticky');
+			    } else {
+			        $('.navContainer').removeClass('sticky'); 
+			    }
+			};
+
+			stickyNav();
+			// and run it again every time you scroll
+			$(window).scroll(function() {
+				stickyNav();
+			});
+		});
