469ff5cc787087c0c2859ccfdffc9022fb1ad7d3
Michał Szyszka <mszyszka223@gmail.com>
2017-12-06 12:33:48 +0100

    I added sticky header for mobile

--- script.js
+++ script.js
@@ -80,3 +80,29 @@
 				stickyNav();
 			});
 		});
+
+ //Sticky header for width less then 992px;
+        $(document).ready(function() {
+			// grab the initial top offset of the navigation 
+		   	var stickyHeaderTop = $('.siteHeader').offset().top;
+		   	
+		   	// our function that decides weather the navigation bar should have "fixed" css position or not.
+		   	var stickyHeader = function(){
+			    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+			         
+			    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+			    // otherwise change it back to relative
+			    if (scrollTop > stickyHeaderTop) { 
+			        $('.siteHeader').addClass('stickyHeader');
+			    } else {
+			        $('.siteHeader').removeClass('stickyHeader'); 
+			    }
+			};
+
+			stickyHeader();
+			// and run it again every time you scroll
+			$(window).scroll(function() {
+				stickyHeader();
+			});
+		});
+        
