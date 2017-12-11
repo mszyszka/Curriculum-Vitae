d20593f5b4ba73208ae14a9b9badf5647edecf7e
Michał Szyszka <mszyszka223@gmail.com>
2017-12-11 13:16:52 +0100

    I fixed sticky nav

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
             x.classList.toggle("change");
             document.getElementsByClassName("navMenu")[0].classList.toggle('navOpen');
         }
+
         //Top button:
         // 1. lets first listen for the scroll event
         $(window).scroll(function () {
@@ -18,6 +19,7 @@
                 $('#topBtn').fadeOut();
             }
         });
+
         // on click:
         $('#topBtn').click(function () {
             $('body,html').animate({
@@ -25,33 +27,37 @@
                 scrollTop: 0
             }, 500);
         });
+
         // scroll to function
         jQuery(function($){
             //scroll reset
             $.scrollTo(0);
             $('#link1').click(function(){
-                $.scrollTo($("#js").offset().top -30,500);   
+                $.scrollTo($("#js").offset().top -210,500);   
             });
         });
+
         jQuery(function($){
             //scroll reset
             $.scrollTo(0);
             $('#link2').click(function(){
-                $.scrollTo($("#css").offset().top -30,600);   
+                $.scrollTo($("#css").offset().top -210,600);   
             });
         });
+
         jQuery(function($){
             //scroll reset
             $.scrollTo(0);
             $('#link3').click(function(){
-                $.scrollTo($("#html").offset().top -30,700);   
+                $.scrollTo($("#html").offset().top -210,700);   
             });
         });
+
         jQuery(function($){
             //scroll reset
             $.scrollTo(0);
             $('#link4').click(function(){
-                $.scrollTo($("#tech").offset().top -30,800);  
+                $.scrollTo($("#tech").offset().top -210,800);  
             });
         });
 
@@ -68,9 +74,9 @@
 			    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
 			    // otherwise change it back to relative
 			    if (scrollTop > stickyNavTop) { 
-			        $('.navContainer').addClass('sticky');
+			        $('.navContainer').addClass('stickyNav');
 			    } else {
-			        $('.navContainer').removeClass('sticky'); 
+			        $('.navContainer').removeClass('stickyNav'); 
 			    }
 			};
 
@@ -82,6 +88,7 @@
 		});
 
  //Sticky header for width less then 992px;
+
         $(document).ready(function() {
 			// grab the initial top offset of the navigation 
 		   	var stickyHeaderTop = $('.siteHeader').offset().top;
@@ -103,6 +110,33 @@
 			// and run it again every time you scroll
 			$(window).scroll(function() {
 				stickyHeader();
+			});
+		});
+
+//Sticky navMenu;
+//Im making it becouse when im opening my menu on sticky header menu is on the  top of the site.
+
+        $(document).ready(function() {
+			// grab the initial bottom offset of the header 
+		   	var stickyHeaderTop = $('.siteHeader').offset().top;
+		   	
+		   	// our function that decides weather the navMenu  should have "fixed" css position or not.
+		   	var stickyNavMenu = function(){
+			    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+			         
+			    // if we've scrolled more than the Header, change navMenu position to fixed to stick to bottom of header,
+			    // otherwise change it back to relative
+			    if (scrollTop > stickyHeaderTop) { 
+			        $('.navMenu').addClass('stickyNavMenu');
+			    } else {
+			        $('.navMenu').removeClass('stickyNavMenu'); 
+			    }
+			};
+
+			stickyNavMenu();
+			// and run it again every time you scroll
+			$(window).scroll(function() {
+				stickyNavMenu();
 			});
 		});
         
